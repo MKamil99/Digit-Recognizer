04105d7e5839aba7974552a79211ff253ca4e2b9
MKamil99 <43967269+MKamil99@users.noreply.github.com>
2020-05-01 00:35:35 +0200

    Added Neural Network

---
+++ NeuralNetwork/NeuralNetwork/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NeuralNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] data = Data.LoadIrises(@"IrisDatabase.txt");
+            Network network = new Network(4, 3, 15, 3, "weights.txt");
+            network.Train(data, 10000);
+
+            //RESULTS OF TRAINING:
+            for (int i = 0; i < data.Length; i++)
+                Data.ClassifyIris(new double[] { data[i][0], data[i][1], data[i][2], data[i][3] }, network);
+
+            Console.ReadKey();
+        }
+    }
+}
